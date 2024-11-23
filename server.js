661b1df61d2c74552073d6237018549309a6a813
zeptional <omarpayman89@gmail.com>
2024-11-23 09:19:02 -0800

    hi

---
+++ server.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server running');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+
+        // Broadcast the message to all clients
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(8080, () => {
+    console.log('WebSocket server running on port 8080');
+});
