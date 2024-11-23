26a8564ce79c72bc63d27139835549da2a5b9e95
zeptional <omarpayman89@gmail.com>
2024-11-23 17:50:36 +0000

    ok

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,6 @@ self.addEventListener('push', (event) => {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
         body: data.message,
-        icon: 'https://cdn.creazilla.com/icons/3247957/notification-icon-md.png'  // Replace with the path to your notification icon
+        icon: '/icon.png'  // Optional: You can add a custom icon for the notification
     });
 });
